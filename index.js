d87ecd966d47a4c683ce89e0635a27a6ab424e16
Luis Salcedo <luis.e.ss@gmail.com>
2023-03-29 16:27:23 -0600

    Complete Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const cards = [];
+
+function writeCards(name, event) {
+  for (let i = 0; i < name.length; i++) {
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return cards;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
